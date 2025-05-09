bdb70a5ef1c8fa82014f3740fbf47c0811a330eb
emilyfranceskolar@gmail.com <emilyfranceskolar@gmail.com>
2025-05-09 18:45:24 +0200

    Små ändringar efter min granskning av lösningsförslaget

--- src/script.js
+++ src/script.js
@@ -12,38 +12,38 @@ let people =[
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
-  let input = document.getElementById("searchInput");
-  let button = document.getElementById("searchBtn");
-  let htmlText = document.getElementById("resultDisplay");
+  const input = document.getElementById("searchInput");
+  const button = document.getElementById("searchBtn");
+  const htmlText = document.getElementById("resultDisplay");
   
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
     // 1. Hämta texten i searchInput.value
-    htmlText.innerHTML = input.value;
+    //htmlText.innerHTML = input.value;
+    const name = input.value;
     // 2. Skapa en boolean för found
     let isFound = false;
   
     // 3. Loopa igenom people-arrayen
-    for (let i = 0; i < people.length; i++){
-      if(input.value === people[i]){
+    for (let person of people){
+    //(let i = 0; i < people.length; i++){
+      if (name === person){
+      //if(input.value === people[i]){
         //console.log(people[i])
-        htmlText.innerHTML = `Namn hittades: ${input.value}`;
+        htmlText.innerHTML = `Namn hittades: ${name}`;
         //console.log("hi")
         isFound = true;
-        break;
-      } else {
+      } else if (!isFound) {
         htmlText.innerHTML = ("Namn hittades inte.");
         isFound = false;
       } 
     }
-  
     // 4. Jämför texten med varje namn i arrayen
     
     // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
     
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-
-    
+ 
     });
 }
 
